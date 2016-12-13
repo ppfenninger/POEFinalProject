2cb338cf1c3d1c577f15546d5faa8af296b11e58
ppfenninger <paige.pfennninger@students.olin.edu>
2016-12-12 23:11:31 -0500

    Sprint3Code/

--- Sprint3Code/Sprint3Code.ino
+++ Sprint3Code/Sprint3Code.ino
@@ -1,18 +1,22 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_MotorShield AFMSbot(0x60); // rotary clock (no jumpers closed)
+Adafruit_MotorShield AFMStop(0x61); // cake clock (rightmost jumper closed)
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
+#include <Servo.h>
+
 // motor and LED setup code
-Adafruit_DCMotor *cakeMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *rotaryMotor = AFMS.getMotor(1);
+Adafruit_StepperMotor *cakeMotor = AFMStop.getStepper(513, 1); //attached to M3 and M4 on the bottom motorshield
+Adafruit_StepperMotor *rotaryMotor = AFMSbot.getStepper(513, 2); //attached to M1 and M2 on the top motershield
+Adafruit_NeoPixel lighthouseLed = Adafruit_NeoPixel(1, 3, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel sensorStrip = Adafruit_NeoPixel(3, 5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel animalStrip = Adafruit_NeoPixel(12, 6, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel animalStrip = Adafruit_NeoPixel(11, 6, NEO_GRB + NEO_KHZ800);
 
 // initialize cake clock's IR distance sensors
 int sensor1Pin = A0;
@@ -22,22 +26,28 @@ int sensor1Value = 0;
 int sensor2Value = 0;
 int sensor3Value = 0;
 
-uint8_t cakeSpeed = 30;       // speed of cakeMotor
 boolean isClockwise = true;   // direction of cakeMotor
 int sensVals[3] = {0, 0, 0};  // array to record order sensors are triggered
 int threshold = 300;          // threshold to trigger distance sensors
 boolean cakeTriggered = false;// if cakeTriggered==true, rotary clock can be triggered
-int animalNumber = -1;        // number of animal that is lit up
 
-//unsigned long lastWalk = 0;   //how long it's been since someone walked around the cake clock
+int animalledCounter = 0;
+int animalNumber = -1;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+unsigned long interval = 500;
+int ledState = 0;
+
+unsigned long lastWalk = 0;   //how long it's been since someone walked around the cake clock
 //int rotaryThreshold = 5000;   //wait 5 seconds after face doesn't move to bring back to 12 position
-//int cakeThreshold = 30000;    //wait 30 seconds after ppl stop walking around to stop moving
+int cakeTimeThreshold = 30000;    //wait 30 seconds after ppl stop walking around to stop moving
 
 // rotary clock variables
-const int enc1Pin = 13;
-const int enc2Pin = 12;
-const int enc3Pin = 11;
-const int enc4Pin = 10;
+boolean rotaryTriggered = false; 
+const int enc1Pin = 10;
+const int enc2Pin = 9;
+const int enc3Pin = 8;
+const int enc4Pin = 7;
 
 // sensor values. enc1 is far outer edge, enc4 is inner edge.
 int enc1 = 0;
@@ -51,24 +61,41 @@ byte lastEncoderValue = B0000;
 
 int clockNumber = 0;          // at 12 position
 
+//krakuation variables
+int krakenPin = A3; 
+int pressurePadValue = 500; //this puts it above the threshhold
+int krakenThreshold = 350; //value read on the kraken pad to indicate that it has been stomped on
+Servo krakenServo; //kraken servo 
+int inKrakenPos = 30; //good values for kracutation
+int outKrakenPos = 55;
+boolean lightHouseTriggered = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  AFMS.begin();
+  AFMSbot.begin();
+  AFMStop.begin();
 
   // cake clock setup code starts LEDs and cakeMotor
   sensorStrip.begin();
   sensorStrip.show();
   animalStrip.begin();
   animalStrip.show();
-  cakeMotor->setSpeed(100);
-  cakeMotor->run(FORWARD);
-  cakeMotor->run(RELEASE);
-
+  cakeMotor->setSpeed(10);
+  rotaryMotor->setSpeed(10);
+  
+//  lighthouse setup
+  lighthouseLed.begin();
+  lighthouseLed.show();
+  krakenServo.attach(11); //kraken servo is attached to pwm pin 11
 }
 
 void loop() {
+  Serial.println("running"); 
+//   puts servo in correct position at beginning
+  if (millis() < 2000) {
+    krakenServo.write(inKrakenPos); 
+  }
   // read distance sensors
   sensor1Value = analogRead(sensor1Pin);
   sensor2Value = analogRead(sensor2Pin);
@@ -126,37 +153,83 @@ void loop() {
     sensVals[1] = 0;
     sensVals[2] = 0;
 
-    if (isClockwise) {
-      cakeMotor->run(FORWARD);
-    }
-    else {
-      cakeMotor->run(BACKWARD);
-    }
-    cakeMotor->setSpeed(cakeSpeed); // turns cake motor on
     cakeTriggered = true;           // cake clock has been triggered
 
-    delay(1000);
+    delay(500);
     // turns on the LEDs corresponding to distance sensors that were triggered
     sensorStrip.setPixelColor(0, 0);
     sensorStrip.setPixelColor(1, 0);
     sensorStrip.setPixelColor(2, 0);
     sensorStrip.show();
 
-    // turns on an LED behind a random animal; animalNumber is now rotary puzzle solution
+    //animalNumber is now rotary puzzle solution
     if (animalNumber < 0) {
       animalNumber = random(11);
       Serial.println(animalNumber);
-      animalStrip.setPixelColor(animalNumber, animalStrip.Color(0, 200, 0));
+      for(int x = 0; x <= 11; x ++) {
+        animalStrip.setPixelColor(x, animalStrip.Color(20, 20, 20));
+      }
       animalStrip.show();
+      animalledCounter = animalNumber;
     }
+
+     lastWalk = millis(); // time that the person last triggered all 3 sensors 
+     Serial.println("cake clock triggered"); 
   }
-  // cakeMotor stops turning after [cakeThreshold] milliseconds
-  //if ((millis() - lastWalk > cakeThreshold)) {
-  //  cakeMotor->setSpeed(0);
-  //}
-  //lastTime = millis();
+
+//turns motor if cake clock has been triggered within cakeTimeThreshold
+if ((millis() - lastWalk < cakeTimeThreshold) && cakeTriggered) {
+  if (isClockwise) {
+    cakeMotor->step(10, FORWARD, SINGLE);
+  }
+  else {
+    cakeMotor->step(10, BACKWARD, SINGLE); 
+  }
+}
   
   if (cakeTriggered) { //runs all of the rotary clock code
+    Serial.print("led counter: ");
+    Serial.println(animalledCounter);
+    Serial.print("animal number: "); 
+    Serial.println(animalNumber);  
+    //makes the LED in the light house blink
+      currentMillis = millis(); 
+      if (animalledCounter < animalNumber) {
+
+        if ((currentMillis - previousMillis) > interval) {
+          previousMillis = millis();
+          if (ledState == 0) {
+            lighthouseLed.setPixelColor(0, lighthouseLed.Color(0, 255, 0));
+            ledState = 1;
+            lighthouseLed.show();
+            Serial.println("led triggered"); 
+          }
+          else {
+            lighthouseLed.setPixelColor(0, lighthouseLed.Color(0, 0, 0));
+            ledState = 0;
+            animalledCounter = animalledCounter + 1;
+            lighthouseLed.show();
+          }
+    
+        }
+    }
+    if ((animalledCounter == (animalNumber))) {
+      if ((currentMillis - previousMillis) > (interval + 2000)) { //creates a long pause
+        if (ledState == 1) {
+          Serial.println("yay1");
+          previousMillis = millis();
+          ledState = 0;
+          animalledCounter = 0;
+        }
+        else { 
+          Serial.println("yay2"); 
+          lighthouseLed.setPixelColor(0, lighthouseLed.Color(0, 0, 0));
+          lighthouseLed.show();
+          previousMillis = millis();
+          ledState = 1;
+        }
+      }
+    }
     // read encoder reflectance sensors
     enc1 = digitalRead(enc1Pin);
     enc2 = digitalRead(enc2Pin);
@@ -203,46 +276,63 @@ void loop() {
       case B1011:
         clockNumber = 11;
         break;
-    }
-    Serial.print(animalNumber);
-    Serial.println(clockNumber);
+  }
     if (clockNumber == animalNumber) { // we have achieved our goal
-      sensorStrip.setPixelColor(0, sensorStrip.Color(0, 0, 50)); // three LEDs turn blue to indicate you have won
+      //turns sensor LEDs blue
+      sensorStrip.setPixelColor(0, sensorStrip.Color(0, 0, 50)); 
       sensorStrip.setPixelColor(1, sensorStrip.Color(0, 0, 50));
       sensorStrip.setPixelColor(2, sensorStrip.Color(0, 0, 50));
       sensorStrip.show();
 
-      // animal LEDs reset
-      animalStrip.setPixelColor(animalNumber, 0);
-      animalNumber = -1;
+      //lighthouse strip turns blue
+      lighthouseLed.setPixelColor(0, lighthouseLed.Color(0, 0, 255));
+      lighthouseLed.show(); 
 
-      // stops cake motor if that hasn't happened already
-      cakeMotor->setSpeed(0);
+      rotaryTriggered = true;
     }
 
-    lastEncoderValue = encoderValue;
-    
-    // THE FOLLOWING CODE IS NOT YET TESTED
-    // checks to see how long it's been since the rotary clock was moved
-    //    if ((millis() - lastTime > rotaryThreshold) && (encoderValue != B0100)) {
-    // rotaryMotor rotates clock face back to 12 position
-    //      rotaryMotor->setSpeed(RSpeed);
-    //
-    //      while (encoderValue != B0100) { // until it reaches the 0 degrees marker
-    //        cakeMotor->run(BACKWARD);
-    //
-    //        //run cakeMotor whichever direction it was going to run anyway
-    //        if (isClockwise) {
-    //          cakeMotor->run(FORWARD);
-    //        }
-    //        else {
-    //          cakeMotor->run(BACKWARD);
-    //        }
-    //      }
-    //
-    //      rotaryMotor->setSpeed(0);
-    //      clockNumber = 0;
-    //      Serial.println("returned to 12");
-    //    }
+    if (rotaryTriggered && (clockNumber != 0)){
+      rotaryMotor->step(10, FORWARD, SINGLE);
+    }
+  } //end of just rotary clock code
+
+//start of krackuation
+
+if (rotaryTriggered && cakeTriggered) {
+  pressurePadValue = analogRead(krakenPin); 
+  
+  if (pressurePadValue >= krakenThreshold) {
+    lighthouseLed.setPixelColor(0, lighthouseLed.Color(255, 100, 0));
+    lighthouseLed.show();
+    delay(1000); // pause before kractuation
+    krakenServo.write(outKrakenPos);
+    delay(200); //wait for the spring to get into position
+    krakenServo.write(inKrakenPos);
+    delay(2000); 
+    lightHouseTriggered = true;
   }
+
+  if (lightHouseTriggered) { //resets everything
+    cakeTriggered = false;
+    rotaryTriggered = false;
+    lightHouseTriggered = false;
+    
+    animalNumber = -1; 
+    sensVals[0] = 0;
+    sensVals[1] = 0;
+    sensVals[2] = 0;
+    
+    //turn all of the LEDs off
+    lighthouseLed.setPixelColor(0, lighthouseLed.Color(0, 0, 0));
+    lighthouseLed.show(); 
+    for(int x = 0; x <= 11; x ++) {
+        animalStrip.setPixelColor(x, animalStrip.Color(0, 0, 0));
+    }
+    animalStrip.show();
+    sensorStrip.setPixelColor(0, 0);
+    sensorStrip.setPixelColor(1, 0);
+    sensorStrip.setPixelColor(2, 0);
+    sensorStrip.show();
+  }
+}
 }
